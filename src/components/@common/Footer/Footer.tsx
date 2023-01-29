4bd396bbebe5ff1ec0c34c9578d4d66f99a01265
ddaisylee <elliejay9988@gmail.com>
2023-01-29 14:58:33 +0900

    ✏️ Fix: Footer 컴포넌트 표현식을 함수 선언문으로 수정

--- src/components/@common/Footer/Footer.tsx
+++ src/components/@common/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import * as S from './Footer.styles';
 
-const Footer = function () {
+function Footer() {
   return (
     <S.Container>
       Thank You for Visiting My Blog, Have a Good Day🌼❤️
       <br />© 2022 Developer DaisyLee, Powered By Gatsby.
     </S.Container>
   );
-};
+}
 
 export default Footer;
