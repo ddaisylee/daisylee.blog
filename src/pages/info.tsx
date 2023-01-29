f60c1192c1a3025280ff84e1c4918f03c4512591
ddaisylee <elliejay9988@gmail.com>
2023-01-29 14:15:55 +0900

    🩹 Chore: 불필요한 주석 삭제

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,13 +1,8 @@
 import React, { FunctionComponent } from 'react';
-//graphql을 사용하기 위해 import합니다.
 import { graphql } from 'gatsby';
-// CSS 정의 및 글로벌 스타일을 지정하기 위해 컴포넌트의 함수를 불러옵니다.
 import { Global, css } from '@emotion/react';
-//CSS 정의가 아닌 Styled Component를 생성하기 위해 함수를 불러옵니다.
 import styled from '@emotion/styled';
 
-//(2) 응답 받은 결과에 대한 타입을 지정해줍니다.
-//data라는 키의 값으로 결과가 전달받는 것에 주의합니다.
 type InfoPageProps = {
   data: {
     site: {
@@ -20,7 +15,6 @@ type InfoPageProps = {
   };
 };
 
-//(3) 함수의 parameter 부분에서 구조 분해 할당을 이용해 값을 추출해 바로 사용해줍니다.
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -40,7 +34,6 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 
 export default InfoPage;
 
-//(1) 원하는 데이터에 대한 쿼리를 변수에 담아 요청을 보냅니다.
 export const metadataQuery = graphql`
   {
     site {
@@ -53,7 +46,6 @@ export const metadataQuery = graphql`
   }
 `;
 
-//1. 컴포넌트의 props 값으로 전달할 CSS를 정의하는 방법
 const globalStyle = css`
   * {
     margin: 0;
