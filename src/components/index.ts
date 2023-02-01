6cb192f09da5987bc6501384f22f20130a9457cf
ddaisylee <elliejay9988@gmail.com>
2023-02-01 14:30:30 +0900

    ✏️ Chore: components 폴더의 index.ts에 컴포넌트 export문 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,9 @@
 export { default as CategoryList } from './CategoryList/CategoryList';
+export { default as CommentWidget } from './CommentWidget/CommentWidget';
 export { default as Introduction } from './Introduction/Introduction';
+export { default as PostContent } from './PostContent/PostContent';
+export { default as PostHead } from './PostHead/PostHead';
+export { default as PostHeadInfo } from './PostHeadInfo/PostHeadInfo';
 export { default as PostItem } from './PostItem/PostItem';
 export { default as PostList } from './PostList/PostList';
 export { default as ProfileImage } from './ProfileImage/ProfileImage';
