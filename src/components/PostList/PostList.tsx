4ab46c3de3458f6f537efccb5f589d4059d6a6ff
ddaisylee <elliejay9988@gmail.com>
2023-02-01 12:09:45 +0900

    ✨ Feat: 카테고리 필터링 기능 추가

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PostItem from '../PostItem/PostItem';
 import { PostListItemType } from 'types/PostItem.types';
 import * as S from './PostList.styles';
 
 type PostListProps = {
+  selectedCategory: string;
   posts: PostListItemType[];
 };
 
-export default function PostList({ posts }: PostListProps) {
+export default function PostList({ selectedCategory, posts }: PostListProps) {
+  const postListData = useMemo(
+    () =>
+      posts.filter(
+        ({
+          node: {
+            frontmatter: { categories },
+          },
+        }: PostListItemType) =>
+          selectedCategory !== 'All'
+            ? categories.includes(selectedCategory)
+            : true,
+      ),
+    [selectedCategory],
+  );
   return (
     <S.Container>
-      {posts.map(
+      {postListData.map(
         ({
           node: {
             id,
